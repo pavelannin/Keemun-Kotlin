e0c3c64a0f44465fc2b5787b846ec57b29da41ca
Pavel Annin <pavelannin.dev@gmail.com>
2023-11-16 23:39:56 +0300

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
-    alias(deps.plugins.android.application) apply  false
-    alias(deps.plugins.android.library) apply  false
-    alias(deps.plugins.kotlin.multiplatform) apply  false
-    alias(deps.plugins.kotlin.cocoapods) apply  false
+    alias(deps.plugins.android.application) apply false
+    alias(deps.plugins.android.library) apply false
+    alias(deps.plugins.kotlin.multiplatform) apply false
+    alias(deps.plugins.kotlin.android) apply false
+    alias(deps.plugins.kotlin.cocoapods) apply false
+    alias(deps.plugins.kotlin.serialization) apply false
 }
 
 tasks.register("clean", Delete::class) {
