1a516e8303e09e5fc68759679d84cca97b8bf3a9
Pavel Annin <pavelannin.dev@gmail.com>
2024-03-03 09:35:54 +0300

    release template plugin 1.0.2

--- plugins/template/build.gradle.kts
+++ plugins/template/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.pavelannin.keemun.template"
-version = "1.0.0"
+version = "1.0.2"
 
 // Required as the "intellij" plugin is overriding the repositories from "settings.gradle"
 repositories {
@@ -15,6 +15,15 @@ repositories {
 intellij {
     pluginName = "Keemun Template"
     version = "2023.1.1"
+    updateSinceUntilBuild.set(true)
     type = "IC"
     plugins.set(listOf("android"))
+}
+
+tasks {
+    patchPluginXml {
+        version.set("1.0.2")
+        sinceBuild.set("231")
+        untilBuild.set("241.*")
+    }
 }
